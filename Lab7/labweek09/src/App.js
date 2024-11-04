0b4afb29e0a1503537f88dd8da129656a977d32d
ammar <ammar.sultan@georgebrown.ca>
2024-11-03 23:11:36 -0500

    Lab7

---
+++ Lab7/labweek09/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p> Student ID: 101296759</p>
+        <p>Full Name: Ammar Sultan</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab7/labweek09/src/components/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <h1>Welcome to React Programming</h1>;
+};
+
+export default Welcome;
